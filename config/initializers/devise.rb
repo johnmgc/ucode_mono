f014028ce7016c82c51918d5c41f28272a9c5df1
John Mario <johnmgc.dev@gmail.com>
2022-05-29 13:47:54 -0500

    cambios en los inicializadores

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -26,6 +26,8 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
+
+
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
@@ -308,4 +310,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+  config.navigational_formats = ['/', :html, :turbo_stream]
 end
