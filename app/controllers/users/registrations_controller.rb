373ed275ecffd5641631c261cf75b463f4622d56
John Mario <johnmgc.dev@gmail.com>
2022-06-04 21:55:01 -0500

    se crean los nicnames de usuarios

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,33 +1,32 @@
-# frozen_string_literal: true
-
+frozen_string_literal: true
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :sign_up_params, only: [:create]
+  before_action :account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
@@ -38,24 +37,36 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
+  end
+
+
+  def account_update_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :current_password)
+  end
+
+
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   # end
 
-  # If you have extra params to permit, append them to the sanitizer.
+  # # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   # end
 
-  # The path used after sign up.
+  # # The path used after sign up.
   # def after_sign_up_path_for(resource)
   #   super(resource)
   # end
 
-  # The path used after sign up for inactive accounts.
+  # # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   resources :comments
   resources :publishes do
     collection do
       post 'likes/:id', to: 'publishes#likes_publish', as: :likes
     end
   end
-
-    devise_for :users, controllers: {
-      sessions: 'users/sessions'
-    }
+  # devise_for :users, :controllers => { registrations: 'registrations' }
+  
 
   root to: 'publishes#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
